1b20077a15d2a6630ab70009423b4bd89796198a
zoeschmitt <zoe4712@gmail.com>
2020-05-10 17:18:26 -0500

    Binary file updates

--- assembler.py
+++ assembler.py
@@ -1,18 +1,34 @@
+"""
+ASSEMBLER
+This program takes an input file full of assembly language code as an argument. It reads the contents and splits
+each line into an array, then iterates through that array creating arrays of each value in a line and
+identifies the proper opcode from there.
+It will write a binary file as output.
+"""
+
 import re
 import sys
 
+"""
+The labels dictionary will be holding the label name and its location.
+"""
 labels = {}
 
 def zerobin(fn):
-	with open("rom.bin", "wb") as binary_file:
-		binary_file.close()
+	with open("pong.ch8", "wb") as file:
+		file.close()
 
 def writebin(fn, b):
-	with open("rom.bin", "ab") as binary_file:
-        	binary_file.write(bytearray(b))
+	with open("pong.ch8", "ab") as file:
+        	file.write(bytearray(b))
 
-zerobin("rom.bin")
+zerobin("pong.ch8")
 
+"""
+This function is called for each line in the input file, it seperates each of the values in
+the original array of lines into their own array. Then it identifies the instruction, registers,
+and values associated if any.
+"""
 def decode_assembly(line):
     lines_split = re.split(" |, |\(|\)", line)
     #print(lines_split)
@@ -26,44 +42,33 @@ def decode_assembly(line):
     reg = ""
     reg2 = ""
     val = ""
-    instruction1 = ""
-    instruction2 = ""
+    addr = ""
 
 
     if lineArgs[0] == "CLS":
-        instruction1 = "00E0"
-        instruction2 = format(0, '03b') + format(0, '03b') + format(14, '03b') + format(0, '03b')
         b = [0x00, 0, 14, 0]
     
     elif lineArgs[0] == "RET":
-        instruction1 = "00EE"
-        instruction2 = format(0, '02b') + format(0, '02b') + format(14, '03b') + format(14, '02b')
         b = [0x00, 0, 14, 14]
 
     elif lineArgs[0] == "SYS":
-        if lineArgs[1][1:] in labels:
-            reg = labels[lineArgs[1][1:]]
-            instruction1 = "0" + str(labels[lineArgs[1][1:]])
-            instruction2 = format(0, '02b') + format(labels[lineArgs[1][1:]], '03b')
-            b = [0x00, 0, reg >> 8, reg & 0xFF]
+        if lineArgs[1][0:] in labels:
+            addr = labels[lineArgs[1][0:]]
+            b = [0x0, addr >> 8, addr >> 8, addr & 0xFF]
         else:
             print("invalid label in SYS line")
 
     elif lineArgs[0] == "JP":
-        if lineArgs[1][1:] in labels:
-            reg = labels[lineArgs[1][1:]]
-            instruction1 = "1" + str(labels[lineArgs[1][1:]])
-            instruction2 = format(1, '02b') + format(labels[lineArgs[1][1:]], '03b')
-            b = [0x01, 0, reg >> 8, reg & 0xFF]
+        if lineArgs[1][0:] in labels:
+            addr = labels[lineArgs[1][0:]]
+            b = [0x1, addr >> 8, addr >> 8, addr & 0xFF]
         else:
             print("invalid label in JP line")
 
     elif lineArgs[0] == "CALL":
-        if lineArgs[1][1:] in labels:
-            reg = labels[lineArgs[1][1:]]
-            instruction1 = "2" + str(labels[lineArgs[1][1:]])
-            instruction2 = format(2, '02b') + format(labels[lineArgs[1][1:]], '03b')
-            b = [0x02, 0, reg >> 8, reg & 0xFF]
+        if lineArgs[1][0:] in labels:
+            addr = labels[lineArgs[1][0:]]
+            b = [0x2, addr >> 8, addr >> 8, addr & 0xFF]
         else:
             print("invalid label in CALL line")
 
@@ -71,31 +76,23 @@ def decode_assembly(line):
         if lineArgs[2][0] == "V":
             reg = int(lineArgs[1][1:])
             reg2 = int(lineArgs[2][1:])
-            instruction1 = "5" + str(reg) + str(reg2) + "0"
-            instruction2 = format(5, '02b') + format(reg, '03b') + format(reg, '03b') + format(0, '02b')
-            b = [0x05, reg, reg2,0]
+            b = [0x5, reg, reg2,0]
 
         elif lineArgs[2][0] != "V":
             reg = int(lineArgs[1][1:])
             val = int(lineArgs[2])
-            instruction2 = format(3, '02b') + format(reg, '03b') + format(val, '03b')
-            instruction1 = "3" + str(reg) + str(val >> 8) + str(val & 0xFF)
-            b = [0x03, reg, val >> 8, val & 0xFF]
+            b = [0x3, reg, val >> 8, val & 0xFF]
 
     elif lineArgs[0] == "SNE":
         if lineArgs[2][0] == "V":
             reg = int(lineArgs[1][1:])
             reg2 = int(lineArgs[2][1:])
-            instruction1 = "9" + str(reg) + str(reg2) + "0"
-            instruction2 = format(9, '02b') + format(reg, '03b') + format(reg2, '03b') + format(0, '02b')
-            b = [0x09, reg, reg2,0]
+            b = [0x9, reg, reg2,0]
 
         elif lineArgs[2][0] != "V":
             reg = int(lineArgs[1][1:])
             val = int(lineArgs[2])
-            instruction2 = format(4, '02b') + format(reg, '03b') + format(val, '03b')
-            instruction1 = "4" + str(reg) + str(val >> 8) + str(val & 0xFF)
-            b = [0x04, reg, val >> 8, val & 0xFF]
+            b = [0x4, reg, val >> 8, val & 0xFF]
 
     elif lineArgs[0] == "LD":
 
@@ -104,73 +101,48 @@ def decode_assembly(line):
             if lineArgs[2][0] == "V":
                 reg = int(lineArgs[1][1:])
                 reg2 = int(lineArgs[2][1:])
-                instruction1 = "8" + str(reg) + str(reg2) + "0"
-                instruction2 = format(8, '02b') + format(reg, '03b') + format(reg, '03b') + format(0, '02b')
-                b = [0x08, reg, reg2, 0]
+                b = [0x8, reg, reg2, 0]
 
             elif lineArgs[2] == "DT":
                 reg = int(lineArgs[1][1:])
-                instruction1 = "F" + str(reg) + "07"
-                instruction2 = format(15, '02b') + format(reg, '03b') + format(0, '02b') + format(7, '02b')
-                b = [0x0F, reg, 0, 7]
+                b = [0xF, reg, 0, 7]
             
             elif lineArgs[2][0] == "K":
                 reg = int(lineArgs[1][1:])
-                instruction1 = "F" + str(reg) + "0A"
-                instruction2 = format(15, '02b') + format(reg, '03b') + format(0, '02b') + format(10, '02b')
-                b = [0x0F, reg, 0, 10]
+                b = [0xF, reg, 0, 10]
 
             elif lineArgs[2] == "[I]":
                 reg = int(lineArgs[1][1:])
-                instruction1 = "F" + str(reg) + "65"
-                instruction2 = format(15, '02b') + format(reg, '03b') + format(6, '02b') + format(5, '02b')
-                b = [0x0F, reg, 6, 5]
+                b = [0xF, reg, 6, 5]
 
             elif lineArgs[2][0] != "V":
                 reg = int(lineArgs[1][1:])
                 val = int(lineArgs[2])
-                instruction2 = format(6, '02b') + format(reg, '03b') + format(val, '03b')
-                instruction1 = "6" + str(reg) + str(val >> 8) + str(val & 0xFF)
-                b = [0x06, reg, val >> 8, val & 0xFF]
+                b = [0x6, reg, val >> 8, val & 0xFF]
 
         elif lineArgs[1][0] == "I":
-            if lineArgs[2][1:] in labels:
-                reg = labels[lineArgs[2][1:]]
-                instruction1 = "A" + str(labels[lineArgs[2][1:]])
-                instruction2 = format(10, '02b') + format(labels[lineArgs[2][1:]], '03b')
-                b = [0x0A, 0, reg >> 8, reg & 0xFF]
-            else:
-                print("invalid label in LD I line")
+            addr = int(lineArgs[2][0:])
+            b = [0xA, addr >> 8, addr >> 8, addr & 0xFF]
 
         elif lineArgs[1] == "[I]":
             reg = int(lineArgs[2][1:])
-            instruction1 = "F" + str(reg) + "55"
-            instruction2 = format(15, '02b') + format(reg, '03b') + format(5, '02b') + format(5, '02b')
-            b = [0x0F, reg, 5, 5]
+            b = [0xF, reg, 5, 5]
 
         elif lineArgs[1] == "F":
             reg = int(lineArgs[2][1:])
-            instruction1 = "F" + str(reg) + "29"
-            instruction2 = format(15, '02b') + format(reg, '03b') + format(2, '02b') + format(9, '02b')
-            b = [0x0F, reg, 2, 9]
+            b = [0xF, reg, 2, 9]
 
         elif lineArgs[1] == "B":
             reg = int(lineArgs[2][1:])
-            instruction1 = "F" + str(reg) + "33"
-            instruction2 = format(15, '02b') + format(reg, '03b') + format(3, '02b') + format(3, '02b')
-            b = [0x0F, reg, 3, 3]
+            b = [0xF, reg, 3, 3]
 
         elif lineArgs[1] == "DT":
             reg = int(lineArgs[2][1:])
-            instruction1 = "F" + str(reg) + "15"
-            instruction2 = format(15, '02b') + format(reg, '03b') + format(1, '02b') + format(5, '02b')
-            b = [0x0F, reg, 1, 5]
+            b = [0xF, reg, 1, 5]
 
         elif lineArgs[1] == "ST":
             reg = int(lineArgs[2][1:])
-            instruction1 = "F" + str(reg) + "18"
-            instruction2 = format(15, '02b') + format(reg, '03b') + format(1, '02b') + format(8, '02b')
-            b = [0x0F, reg, 1, 8]
+            b = [0xF, reg, 1, 8]
 
     elif lineArgs[0] == "ADD":
         if lineArgs[1][0] == "V":
@@ -178,106 +150,80 @@ def decode_assembly(line):
             if lineArgs[2][0] == "V":
                 reg = int(lineArgs[1][1:])
                 reg2 = int(lineArgs[2][1:])
-                instruction1 = "8" + str(reg) + str(reg2) + "4"
-                instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(4, '02b')
-                b = [0x08, reg, reg2, 4]
+                b = [0x8, reg, reg2, 4]
 
             elif lineArgs[2][0] != "V":
                 reg = int(lineArgs[1][1:])
                 val = int(lineArgs[2])
-                instruction1 = "7" + str(reg) + str(val >> 8) + str(val & 0xFF)
-                instruction2 = format(7, '02b') + format(reg, '03b') + format(val, '03b')
-                b = [0x07, reg, val >> 8, val & 0xFF]
+                b = [0x7, reg, val >> 8, val & 0xFF]
 
         if lineArgs[1][0] == "I":
             reg = int(lineArgs[2][1:])
-            instruction1 = "F" + str(reg) + "1E"
-            instruction2 = format(15, '02b') + format(reg, '03b') + format(1, '02b') + format(14, '02b')
-            b = [0x0F, reg, 1, 14]
+            b = [0xF, reg, 1, 14]
 
     elif lineArgs[0] == "OR":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "1"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(1, '02b')
-        b = [0x08, reg, reg2, 1]
+        b = [0x8, reg, reg2, 1]
 
     elif lineArgs[0] == "AND":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "2"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(2, '02b')
-        b = [0x08, reg, reg2, 2]
+        b = [0x8, reg, reg2, 2]
 
     elif lineArgs[0] == "XOR":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "3"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(3, '02b')
-        b = [0x08, reg, reg2, 3]
+        b = [0x8, reg, reg2, 3]
 
     elif lineArgs[0] == "SUB":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "5"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(5, '02b')
-        b = [0x08, reg, reg2, 5]
+        b = [0x8, reg, reg2, 5]
 
     elif lineArgs[0] == "SHR":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "6"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(6, '02b')
-        b = [0x08, reg, reg2, 6]
+        b = [0x8, reg, reg2, 6]
 
     elif lineArgs[0] == "SUBN":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "7"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(7, '02b')
         b = [0x08, reg, reg2, 7]
 
     elif lineArgs[0] == "SHL":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
-        instruction1 = "8" + str(reg) + str(reg2) + "E"
-        instruction2 = format(8, '02b') + format(reg, '03b') + format(reg2, '03b') + format(14, '02b')
-        b = [0x08, reg, reg2, 14]
+        b = [0x8, reg, reg2, 14]
 
     elif lineArgs[0] == "RND":
         reg = int(lineArgs[1][1:])
         val = int(lineArgs[2])
-        instruction2 = format(12, '02b') + format(reg, '03b') + format(val, '03b')
-        instruction1 = "C" + str(reg) + str(val >> 8) + str(val & 0xFF)
-        b = [0x0C, reg, val >> 8, val & 0xFF]
+        b = [0xC, reg, val >> 8, val & 0xFF]
 
     elif lineArgs[0] == "DRW":
         reg = int(lineArgs[1][1:])
         reg2 = int(lineArgs[2][1:])
         n = int(lineArgs[3])
-        instruction1 = "D" + str(reg) + str(reg2) + str(n)
-        instruction2 = format(13, '02b') + format(reg, '03b') + format(reg2, '03b') + format(n, '03b')
-        b = [0x0D, reg, reg2, n]
+        b = [0xD, reg, reg2, n]
 
     elif lineArgs[0] == "SKP":
         reg = int(lineArgs[1][1:])
-        instruction1 = "E" + str(reg) + "9E"
-        instruction2 = format(14, '02b') + format(reg, '03b') + format(9, '02b') + format(14, '02b')
-        b = [0x0E, reg, 9, 14]
+        b = [0xE, reg, 9, 14]
 
     elif lineArgs[0] == "SKNP":
         reg = int(lineArgs[1][1:])
-        instruction1 = "E" + str(reg) + "A1"
-        instruction2 = format(14, '02b') + format(reg, '03b') + format(10, '02b') + format(1, '02b')
-        b = [0x0E, reg, 10, 1]
+        b = [0xE, reg, 10, 1]
 
-    print("unformatted: " + instruction1)
-    print("formatted: " + format(int(instruction2, 2), '04x'))
     print(b)
-    writebin("rom.bin", b)
 
+    writebin("pong.ch8", b)
 
-    
+"""
+This code runs first. It takes the arugment from the command line (input file), reads the input file contents
+into an array, and then runs through the lines to record the labels and their locations. It then calls decode_assembly
+for each line, making sure to skip label lines in the process.
+"""
 with open(sys.argv[1], 'r') as fp:
     lineNum = 0
     content = fp.read()
@@ -288,7 +234,7 @@ with open(sys.argv[1], 'r') as fp:
             labels[lines[line][1:]] = lineNum * 4
         else: 
             lineNum = lineNum + 1
-    
+    print(labels)
     for line in range (len(lines)):
         if lines[line][0] == ".":
             continue
